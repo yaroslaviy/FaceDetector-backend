6243d2386005463da01947a2b6fe23e988dc02cf
yaroslaviy <yaselokeev@gmail.com>
2019-11-17 17:30:18 -0500

    fixed entries in image

--- controllers/image.js
+++ controllers/image.js
@@ -4,11 +4,14 @@ const MY_KEY = require('../apikey');
 const app = new Clarifai.App({apiKey: MY_KEY});
 
 const handleAPI = (req,res) => {
+    console.log(MY_KEY);
     app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
     .then(data => {
         res.json(data);
     })
-    .catch(err => res.status(400).json('Problem wit api'))
+    .catch(err => {
+        //console.log(err);
+        res.status(400).json('Problem with api')})
 
 }
 
@@ -16,7 +19,7 @@ const handleAPI = (req,res) => {
 const handleImage = (req,res,db) => {
     const { id } = req.body;
     db('users').where('id','=', id)
-    .increment('entries', 1).returning('entries').then(entries => console.log(entries))
+    .increment('entries', 1).returning('entries').then(entries => res.json(entries[0]))
     .catch(err => res.status(400).json('unable to get entries'));
 }
 module.exports =  {
